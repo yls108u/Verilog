b610adf9289d2aca152dbbfc29c91d4719db5e53
yls108u <86284209+yls108u@users.noreply.github.com>
2022-07-05 16:10:42 +0800

    Add files via upload

---
+++ lab3/top.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+module top(
+    input CLK100MHZ,//clk
+    input [15:0]SW,
+    input BTNC,
+    output [5:0]LED
+    );
+    //wire [3:0] a, b, c, d;
+    //wire [5:0] sum;
+    // assign a = SW[3:0];
+    // assign b = SW[7:4];
+    // assign c = SW[11:8];
+    // assign d = SW[15:12];
+    
+    /*****your design******/
+    
+
+    /**********************/
+endmodule
